20ab188968a7de971be2430e02b546a05a37bc78
Sneharshitha10 <sneharshitha.muddasani@hiringhood.com>
2025-02-18 14:41:29 +0530

    today'supdate

---
+++ W2D2_18_Feb/day2practice.js
@@ -0,0 +1,60 @@
+/* functions */
+/* function greet()
+{
+    console.log("hello world");
+}
+greet(); */
+
+/* function greet(user)
+{
+    return "hello ${user}";
+}
+let user="Sneha";
+let str = greet(user);
+console.log(str); */
+
+
+
+
+
+
+/* arrow function */
+
+/* let add = (num1,num2) => {
+    return num1+num2;
+}
+
+let result = add(5,6);
+console.log(result); */
+
+
+
+
+
+
+/* recursion */
+/* function fact(n)
+{
+    if(n == 0)
+        return 1;
+    else
+        return n * fact(n-1);
+}
+
+let num=5;
+let result = fact(num);
+console.log(result);  */
+
+
+
+/* builtin methods for calculations */
+let x = 3.14;
+console.log(Math.round(x));
+console.log(Math.floor(x));
+console.log(Math.ceil(x));
+console.log(Math.pow(x,2));
+console.log(Math.sqrt(x));
+
+
+
+
